0762925497d26f15c728c5f37a5fd720470d2186
CarOL <32268783+hx-w@users.noreply.github.com>
2022-04-18 15:31:25 +0800

    Update utils.go

--- internal/parser/utils.go
+++ internal/parser/utils.go
@@ -85,7 +85,7 @@ func parsePlayerFrame(player *common.Player, addonButton int32, tickrate float64
 
 	lastIdx := len(encoder.PlayerFramesMap[player.Name]) - 1
 	// addons
-	if fullsnap || (lastIdx < 2000 && (lastIdx+1)%int32(tickrate) == 0) || (lastIdx >= 2000 && (lastIdx+1)%int32(tickrate) == 0) {
+	if fullsnap || (lastIdx < 2000 && (lastIdx+1)%int(tickrate) == 0) || (lastIdx >= 2000 && (lastIdx+1)%int(tickrate) == 0) {
 		// if false {
 		iFrameInfo.AdditionalFields |= encoder.FIELDS_ORIGIN
 		iFrameInfo.AtOrigin[0] = float32(player.Position().X)
